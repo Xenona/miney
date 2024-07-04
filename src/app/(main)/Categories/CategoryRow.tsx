e50e10a67478a993387930505a71df77cf281323
Xenona <xenona11x@gmail.com>
2024-07-04 20:25:25 +0300

    Refactored CategoryRow

--- src/app/(main)/Categories/CategoryRow.tsx
+++ src/app/(main)/Categories/CategoryRow.tsx
@@ -1,7 +1,10 @@
 import { Category, updateCategory } from "@/db/category";
 import { useState } from "react";
 import useInput from "../../lib/hooks/useInput";
-import { IRawNotification, useNotificationManager } from "../../lib/NotificationManager/NotificationManager";
+import {
+  IRawNotification,
+  useNotificationManager,
+} from "../../lib/NotificationManager/NotificationManager";
 import { validateNameOrError } from "./AddCategoryForm";
 
 interface ICategoryRowProps {
@@ -13,60 +16,93 @@ export default function CategoryRow({ category, onDelete }: ICategoryRowProps) {
   const notificationManager = useNotificationManager();
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [name, setName] = useState<string>(category.name);
-
   const nameInput = useInput({ type: "text", initialValue: name });
+
+  const acceptEditing = async () => {
+    const nameOrErrors: string | IRawNotification[] = validateNameOrError(
+      nameInput.value,
+    );
+    if (Array.isArray(nameOrErrors)) {
+      notificationManager.addNotifications(...nameOrErrors);
+      return;
+    }
+    const validName: string = nameOrErrors;
+
+    const res = await updateCategory(category.id, validName);
+    if (res.status === "ok") {
+      setName(res.data.name);
+      setIsEditing(false);
+    } else {
+      // res.status === "error";
+      notificationManager.addNotification({
+        type: "Error",
+        body: "Could not update the category name.",
+        devInfo: res.message,
+      });
+    }
+  };
+
   return (
     <div key={category.id}>
-      <div>
-        <p>{category.id}</p>
-      </div>
       {isEditing ? (
-        <>
-          <div>{nameInput.input}</div>
-          <div>
-            <button
-              onClick={async () => {
-                const nameOrErrors: string | IRawNotification[] = validateNameOrError(nameInput.value);
-                if (Array.isArray(nameOrErrors)) {
-                  notificationManager.addNotifications(...nameOrErrors);
-                  return;
-                }
-                const validName: string = nameOrErrors;
-
-                const res = await updateCategory(category.id, validName);
-                if (res.status === 'ok') {
-                  setName(res.data.name);
-                  setIsEditing(false);
-                } else {
-                  // res.status === "error";
-                  notificationManager.addNotification({
-                    type: 'Error',
-                    body: "Could not",
-                    devInfo: res.message
-                  })  
-                }
-              }}
-            >
-              ✓
-            </button>
-          </div>
-          <div>
-            <button onClick={() => setIsEditing(false)}>⨯</button>
-          </div>
-        </>
+        <EditingRow
+          acceptEditing={acceptEditing}
+          cancelEditing={() => setIsEditing(false)}
+        >
+          {nameInput.input}
+        </EditingRow>
       ) : (
-        <>
-          <div>
-            <p>{name}</p>
-          </div>
-          <div>
-            <button onClick={() => setIsEditing(true)}>🖉</button>
-          </div>
-          <div>
-            <button onClick={() => onDelete(category.id, category.name)}>🗑</button>
-          </div>
-        </>
+        <StaticRow
+          startEditing={() => setIsEditing(true)}
+          handleDelete={() => onDelete(category.id, category.name)}
+        >
+          <p>{name}</p>
+        </StaticRow>
       )}
     </div>
+  );
+}
+
+interface IStaticRowProps {
+  children: React.ReactNode;
+  startEditing: () => void;
+  handleDelete: () => void;
+}
+
+function StaticRow({ children, startEditing, handleDelete }: IStaticRowProps) {
+  return (
+    <>
+      <div>{children}</div>
+      <div>
+        <button onClick={startEditing}>🖉</button>
+      </div>
+      <div>
+        <button onClick={handleDelete}>🗑</button>
+      </div>
+    </>
+  );
+}
+
+interface IEditingRowProps {
+  children: React.ReactNode;
+  acceptEditing: () => void;
+  cancelEditing: () => void;
+}
+
+function EditingRow({
+  children,
+  acceptEditing,
+  cancelEditing,
+}: IEditingRowProps) {
+  return (
+    <>
+      <div>{children}</div>
+      <div>
+        <button onClick={acceptEditing}>✓</button>
+      </div>
+      <div>
+        <button onClick={cancelEditing}>⨯</button>
+      </div>
+    </>
   );
 }
--- src/app/lib/hooks/useInput.tsx
+++ src/app/lib/hooks/useInput.tsx
@@ -1,22 +1,27 @@
 import { useState } from "react";
 
 interface IUseFormProps {
-  type: string, 
-  initialValue?: string,
-  [key: string]: any,
+  type: string;
+  initialValue?: string;
+  [key: string]: any;
 }
 
-export default function useInput({type, initialValue="", ...props}: IUseFormProps) {
+export default function useInput({
+  type,
+  initialValue = "",
+  ...props
+}: IUseFormProps) {
   const [value, setValue] = useState<string>(initialValue);
-  
+
   const input = (
     <input
-    type={type}
-    value={value}
-    onChange={(e) => setValue(e.target.value)}
-    {...props}
+      autoFocus
+      type={type}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      {...props}
     />
-  )
-  
-  return {value, setValue, input}
+  );
+
+  return { value, setValue, input };
 }
